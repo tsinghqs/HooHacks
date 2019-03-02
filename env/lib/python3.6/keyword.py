444d537f381a16a8f92c8dc5adc8f56eb2a6c31a
tsinghqs <tsingh2324@gmail.com>
2019-03-02 17:30:56 -0500

    Update desu

---
+++ env/lib/python3.6/keyword.py
@@ -0,0 +1,1 @@
+/Users/tsingh/anaconda3/lib/python3.6/keyword.py
--- natLang.py
+++ natLang.py
@@ -1,14 +1,17 @@
 from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
 
 def language_analysis(text):
-    client = language.Client()
-    sent_analysis = document.analyze_sentiment()
-    print(dir(sent_analysis))
-    sentiment = sent_analysis.sentiment
-    print(sentiment)
-    return sentiment
+    client = language.LanguageServiceClient()
+    document = types.Document(content=text, type=enums.Document.Type.PLAIN_TEXT)
+    sent_analysis = client.analyze_sentiment(document=document).document_sentiment
+    #print(dir(sent_analysis))
+    #sentiment = sent_analysis.sentiment
+    #print(sentiment)
+    return sent_analysis
 
-example_text = "I am sad depression sad boy very sad depressed i get bullied"
+example_text = "I am depressed"
 
 sentiment = language_analysis(example_text)
 print(sentiment.score, sentiment.magnitude)
